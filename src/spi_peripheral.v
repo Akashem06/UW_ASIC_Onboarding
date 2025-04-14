3065c58f6bcf5243ed35b757be6ed29c89e39514
Aryan Kashem <aryankashem@gmail.com>
2025-04-13 22:19:45 -0400

    Added transaction complete flag for synchronization issues

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -40,6 +40,8 @@ reg spi_sclk_sync_0, spi_sclk_sync_1;
 reg spi_cs_sync_0, spi_cs_sync_1;
 reg spi_mosi_sync_0, spi_mosi_sync_1;
 
+reg transaction_complete;
+
 // The sync1 is raeding sample x[n-1]. sync 0 is reading sample x[n]
 wire sclk_rising_edge = ~spi_sclk_sync_1 & spi_sclk_sync_0;
 wire sclk_falling_edge = spi_sclk_sync_1 & ~spi_sclk_sync_0;
@@ -87,24 +89,27 @@ always @(posedge clk or negedge rst_n) begin
 
             SPI_STATE_RECEIVE: begin
                 // On 16th bit we write and update register value. Then enter idle
-                if (bit_count == 4'd15) begin
+                if (transaction_complete) begin
                     // On 16th bit we write and update register value
                     if (read_write_bit == 1'b1) begin
                         if (reg_address <= MAX_ADDRESS) begin
                             case (reg_address)
-                                7'd0: en_reg_out_7_0    <= {shift_reg_in[6:0], spi_mosi_sync_1};
-                                7'd1: en_reg_out_15_8   <= {shift_reg_in[6:0], spi_mosi_sync_1};
-                                7'd2: en_reg_pwm_7_0    <= {shift_reg_in[6:0], spi_mosi_sync_1};
-                                7'd3: en_reg_pwm_15_8   <= {shift_reg_in[6:0], spi_mosi_sync_1};
-                                7'd4: pwm_duty_cycle    <= {shift_reg_in[6:0], spi_mosi_sync_1};
+                                7'd0: en_reg_out_7_0    <= shift_reg_in[7:0];
+                                7'd1: en_reg_out_15_8   <= shift_reg_in[7:0];
+                                7'd2: en_reg_pwm_7_0    <= shift_reg_in[7:0];
+                                7'd3: en_reg_pwm_15_8   <= shift_reg_in[7:0];
+                                7'd4: pwm_duty_cycle    <= shift_reg_in[7:0];
                                 default:;
                             endcase
                         end
                     end
 
+                    transaction_complete <= 1'b0;
                     state <= SPI_STATE_IDLE;
                 end
             end
+
+            default:;
         endcase
     end
 end
@@ -121,10 +126,13 @@ always @(posedge clk or negedge rst_n) begin
         en_reg_pwm_7_0 <= 8'b0;
         en_reg_pwm_15_8 <= 8'b0;
         pwm_duty_cycle <= 8'b0;
+
+        transaction_complete <= 1'b0;
     end else begin
         case (state)
             SPI_STATE_IDLE: begin
                 bit_count <= 4'd0;
+                transaction_complete <= 1'b0;
             end
 
             SPI_STATE_RECEIVE: begin
@@ -139,8 +147,14 @@ always @(posedge clk or negedge rst_n) begin
                         read_write_bit <= spi_mosi_sync_1;
                     else if (bit_count >= 4'd1 && bit_count <= 4'd7)
                         reg_address <= {reg_address[5:0], spi_mosi_sync_1};
+
+                    if (bit_count == 4'd15) begin
+                        transaction_complete <= 1'b1;
+                    end
                 end
             end
+
+            default:;
 
         endcase
     end
